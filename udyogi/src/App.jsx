d2a8c170ec49ef255e816c9a5db4bcf17f613eca
kalavadhillirao34 <kalavadhillirao34@gmail.com>
2025-10-27 21:41:02 +0530

    Added Background images

--- udyogi/src/App.jsx
+++ udyogi/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import LoginPage from './pages/login/login'
 import SignupPage from './pages/signup/signup'
+import Welcome from './pages/Welcome/Welcome';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/Welcome" element={<Welcome/>} />
       </Routes>
     </Router>
   )
