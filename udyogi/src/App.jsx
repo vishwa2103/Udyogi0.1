d90b7263896d0c58e703977d8025daf53145d0c6
vamsikrishna003 <krishnavamc003@gmail.com>
2025-10-27 22:08:26 +0530

    commiting worker dashboard

--- udyogi/src/App.jsx
+++ udyogi/src/App.jsx
@@ -1,21 +1,48 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import './App.css'
+// import LoginPage from './pages/login/login'
+// import SignupPage from './pages/signup/signup'
+// import Navbar from './pages/components/navbar';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   //<WelcomeDashboard />
+  
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Navbar />} />
+//         {/* <Route path="/login" element={<LoginPage />} />
+//         <Route path="/signup" element={<SignupPage />} />
+//         <Route path="/WorkerDashboard" element={<h1>Worker Dashboard</h1>} /> */}
+//       </Routes>
+//     </Router>
+//   )
+// }
+
+// export default App
+
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import LoginPage from './pages/login/login'
-import SignupPage from './pages/signup/signup'
+import Home from "./pages/Home";
+import Employee from "./pages/Employee";
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/employee" element={<Employee />} />
+        <Route path="/employer" element={<><Navbar /><div style={{padding:'4rem'}}>Employer Page</div></>} />
+        <Route path="/notifications" element={<><Navbar /><div style={{padding:'4rem'}}>Notifications Page</div></>} />
+        <Route path="/profile" element={<><Navbar /><div style={{padding:'4rem'}}>Profile Page</div></>} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ udyogi/src/pages/Employee.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import EmployeeOverview from "../components/EmployeeOverview";
+import "./Employee.css";
+import JobRecommendations from "../components/JobRecommendations";
+import AvailableJobs from "../components/AvailableJobs";
+import AvailableJobsSection from "../components/AvailableJobsSection";
+import Footer from "../components/Footer";
+
+const Employee = () => {
+  return (
+    <>
+      <Navbar />
+      <EmployeeOverview />
+      <JobRecommendations />
+      <AvailableJobsSection />
+      <Footer />
+    </>
+  );
+};
+
+export default Employee;
