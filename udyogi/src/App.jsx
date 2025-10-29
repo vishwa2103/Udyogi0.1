cf085647965fb7ca9db2a2a957063a6af4da48ff
kalavadhillirao34 <kalavadhillirao34@gmail.com>
2025-10-29 21:44:41 +0530

    Profile added

--- udyogi/src/App.jsx
+++ udyogi/src/App.jsx
@@ -1,23 +1,26 @@
 // import { useState } from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import LoginPage from './pages/login/login'
 import SignupPage from './pages/signup/signup'
 import Home from './pages/home/home'
-import Welcome from './pages/welcome/welcome'
+import Welcome from './pages/Welcome/Welcome';
 import MerchantDashboard from './pages/merchantprofile/MerchantDashboard';
+import Profile from './pages/Profile/Profile';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome/>} />
+        <Route path="/Welcome" element={<Welcome/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/home" element={<Home/>}/>
         <Route path="/merchantdashboard" element={<MerchantDashboard/>}/>
+        <Route path='/' element = {<Profile/>}/>
       </Routes>
     </Router>
   )
